c5c86a33878230395cf70add683cf997fcf96b1a
mbxjy4 <joshuayoung1309@gmail.com>
2025-02-06 14:31:23 +0000

    Add files via upload

---
+++ Quast_Scripts/quast_5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --partition=defq
+#SBATCH --nodes=8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=300g
+#SBATCH --time=48:00:00
+#SBATCH --job-name=quast_5
+#SBATCH --output=/share/BioinfMSc/rotation1/group2/merged_files/logs/slurm-%x-%j.out
+#SBATCH --error=/share/BioinfMSc/rotation1/group2/merged_files/logs/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mbxjy4@exmail.nottingham.ac.uk
+
+source $HOME/.bash_profile
+
+conda activate Quast
+
+quast /share/BioinfMSc/rotation1/group2/merged_files/pass_fastq/assembly/assembly.fasta \
+/share/BioinfMSc/rotation1/group2/merged_short/assembly/assembly.fasta \
+/share/BioinfMSc/rotation1/group2/hybrid/assembly/assembly.fasta \
+-r /share/BioinfMSc/rotation1/group2/quast/refcombined.fasta -o quast_5
+
+conda deactivate
